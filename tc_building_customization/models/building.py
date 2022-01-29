9fb31ef70463879b87a8b767213449f5119bbbca
laminemhedhbi <mohamed.amine.mhedhbi@gmail.com>
2022-01-29 08:55:10 +0100

    First Commit

--- tc_building_customization/models/building.py
+++ tc_building_customization/models/building.py
@@ -5,12 +5,12 @@ class PropertyManagment(models.Model):
     _name = 'managment.building'
     _description = 'Building Record'
 
-    name_property = fields.Char(string="Name of Property", required=True)
+    name_property = fields.Char(string="Name of Property")
     gps_coordinates = fields.Integer(string="GPS Coordinates")
-    nbr_blocks = fields.Integer(string="Nbr of Blocks", required=True)
-    nbr_floors_block = fields.Integer(string="Nbr of Floors by Block", required=True)
-    nbr_units_floor = fields.Integer(string="Nbr of Units by Floor", required=True)
-    nbr_stores_ground_floor = fields.Integer(string="Nbr of Stores on the Ground Floor", required=True)
-    nbr_parking_places = fields.Integer(string="Nbr of Parking Places", required=True)
-    nbr_cellars = fields.Integer(string="Nbr of Cellars", required=True)
+    nbr_blocks = fields.Integer(string="Nbr of Blocks")
+    nbr_floors_block = fields.Integer(string="Nbr of Floors by Block")
+    nbr_units_floor = fields.Integer(string="Nbr of Units by Floor")
+    nbr_stores_ground_floor = fields.Integer(string="Nbr of Stores on the Ground Floor")
+    nbr_parking_places = fields.Integer(string="Nbr of Parking Places")
+    nbr_cellars = fields.Integer(string="Nbr of Cellars")
     unit_type = fields.Char(string="Unit Type" )
